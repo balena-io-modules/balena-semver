8b344802abc078f069551da757cc32211a3153f2
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-07-20 17:15:46 +0100

    Add a resin specific, safe version of `compare`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
 "use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
 exports.__esModule = true;
-__export(require("semver"));
+var semver = require("semver");
+var memoize = require("lodash.memoize");
+var trimOsText = function (version) {
+    return version.replace(/^resin\sos\s/gi, '')
+        .replace(/\(\W\)$/, '');
+};
+var safeSemver = function (version) {
+    return version.replace(/(\.[0-9]+)\.rev/, '$1+rev');
+};
+var getRev = function (osVersion) {
+    var rev = semver.parse(osVersion).build.map(function (metadataPart) {
+        var matches = /rev(\d+)/.exec(metadataPart);
+        return matches && matches[0] || null;
+    })
+        .filter(function (x) { return x != null; })[0];
+    if (rev != null) {
+        return parseInt(rev, 10);
+    }
+    else {
+        return 0;
+    }
+};
+var isDevelopmentVersion = function (version) {
+    return /(\.|\+|-)dev/.test(version);
+};
+exports.compare = memoize(function (versionA, versionB) {
+    var isAValid = semver.valid(versionA);
+    var isBValid = semver.valid(versionB);
+    if (isAValid && !isBValid) {
+        return 1;
+    }
+    if (!isAValid && isBValid) {
+        return -1;
+    }
+    if (!isAValid && !isBValid) {
+        if (versionA > versionB) {
+            return -1;
+        }
+        if (versionA < versionB) {
+            return 1;
+        }
+        return 0;
+    }
+    versionA = trimOsText(safeSemver(versionA));
+    versionB = trimOsText(safeSemver(versionB));
+    var semverResult = semver.rcompare(versionA, versionB);
+    if (semverResult !== 0) {
+        return semverResult;
+    }
+    var revA = getRev(versionA);
+    var revB = getRev(versionB);
+    if (revA !== revB) {
+        return revB - revA;
+    }
+    var devA = Number(isDevelopmentVersion(versionA));
+    var devB = Number(isDevelopmentVersion(versionB));
+    if (devA !== devB) {
+        return devA - devB;
+    }
+    return versionA.localeCompare(versionB);
+}, function (a, b) { return a + " && " + b; });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,71 @@
-export * from 'semver';
+import * as semver from 'semver';
+import memoize = require('lodash.memoize');
+
+// TODO: PR to DefinitelyTyped project
+declare module 'semver' {
+	function parse(version: string, loose?: boolean): semver.SemVer;
+}
+
+const trimOsText = (version: string) => {
+	return version.replace(/^resin\sos\s/gi, '')
+	.replace(/\(\W\)$/, '');
+};
+
+const safeSemver = (version: string) => {
+	return version.replace(/(\.[0-9]+)\.rev/, '$1+rev');
+};
+
+const getRev = (osVersion: string) => {
+	const rev = semver.parse(osVersion).build.map(function(metadataPart) {
+		const matches = /rev(\d+)/.exec(metadataPart);
+		return matches && matches[0] || null;
+	})
+	.filter((x) => x != null)[0];
+
+	if (rev != null) {
+		return parseInt(rev, 10);
+	} else {
+		return 0;
+	}
+};
+
+const isDevelopmentVersion = (version: string) => {
+	return /(\.|\+|-)dev/.test(version);
+};
+
+export const compare = memoize((versionA: string, versionB: string) => {
+	const isAValid = semver.valid(versionA);
+	const isBValid = semver.valid(versionB);
+	if (isAValid && !isBValid) {
+		return 1;
+	}
+	if (!isAValid && isBValid) {
+		return -1;
+	}
+	if (!isAValid && !isBValid) {
+		if (versionA > versionB) {
+			return -1;
+		}
+		if (versionA < versionB) {
+			return 1;
+		}
+		return 0;
+	}
+	versionA = trimOsText(safeSemver(versionA));
+	versionB = trimOsText(safeSemver(versionB));
+	const semverResult = semver.rcompare(versionA, versionB);
+	if (semverResult !== 0) {
+		return semverResult;
+	}
+	const revA = getRev(versionA);
+	const revB = getRev(versionB);
+	if (revA !== revB) {
+		return revB - revA;
+	}
+	const devA = Number(isDevelopmentVersion(versionA));
+	const devB = Number(isDevelopmentVersion(versionB));
+	if (devA !== devB) {
+		return devA - devB;
+	}
+	return versionA.localeCompare(versionB);
+}, (a: string, b: string) => `${a} && ${b}`);
