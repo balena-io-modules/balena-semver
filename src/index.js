1859eb66e8a4a40c9f63e0a9922745f59f424120
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-07-31 12:19:02 +0100

    Add a `.gte()` method.
    Added comprehensive testing to `.compare()` method and fix a number of bugs in
    `.compare()` method and `getRev()` helper function.
    Previously, `.compare()` incorrectly performed a right compare, causing the results to be
    the reverse of what is expected.
    
    Connects to #15
    
    change-type: minor

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ exports.__esModule = true;
 var semver = require("semver");
 var memoize = require("lodash.memoize");
 var trimOsText = function (version) {
-    return version.replace(/^resin\sos\s/gi, '')
-        .replace(/\s+\(\w+\)$/, '');
+    // Remove "Resin OS" text
+    return version.replace(/^resin\sos\s+/gi, '')
+        .replace(/\s+\(\w+\)$/, '')
+        .replace(/^v/, '');
 };
 var safeSemver = function (version) {
     return version.replace(/(\.[0-9]+)\.rev/, '$1+rev');
 };
+var normalize = function (version) { return trimOsText(safeSemver(version)); };
 var getRev = function (osVersion) {
     var rev = semver.parse(osVersion).build.map(function (metadataPart) {
         var matches = /rev(\d+)/.exec(metadataPart);
-        return matches && matches[0] || null;
+        return matches && matches[1] || null;
     })
         .filter(function (x) { return x != null; })[0];
     if (rev != null) {
@@ -37,9 +40,12 @@ var isDevelopmentVersion = function (version) {
  * @param {string|null} versionA - The first version to compare
  * @param {string|null} versionB - The second version to compare
  *
- * @returns {number} one of `1`, `0`, or `-1`. null values are always weighted below
- * string values, and string values are always weighted below valid semver values.
- * If both values are invalid semver values, then the values are compared alphabetically
+ * @returns {number} Returns `0` if `versionA == versionB`,
+ * or `1` if `versionA` is greater, or `-1` if `versionB` is greater. Sorts in ascending
+ * order if passed to `Array.sort()`.
+ * null values are always weighted below string values, and string values are always
+ * weighted below valid semver values.
+ * If both values are invalid semver values, then the values are compared alphabetically.
  *
  * @example
  * resinSemver.compare(null, 'Resin OS 2.0.0+rev4 (prod)'); //--> -1
@@ -64,8 +70,8 @@ exports.compare = memoize(function (versionA, versionB) {
     if (versionA !== null && versionB === null) {
         return 1;
     }
-    versionA = versionA;
-    versionB = versionB;
+    versionA = normalize(versionA);
+    versionB = normalize(versionB);
     var isAValid = semver.valid(versionA);
     var isBValid = semver.valid(versionB);
     if (isAValid && !isBValid) {
@@ -76,28 +82,26 @@ exports.compare = memoize(function (versionA, versionB) {
     }
     if (!isAValid && !isBValid) {
         if (versionA > versionB) {
-            return -1;
+            return 1;
         }
         if (versionA < versionB) {
-            return 1;
+            return -1;
         }
         return 0;
     }
-    versionA = trimOsText(safeSemver(versionA));
-    versionB = trimOsText(safeSemver(versionB));
-    var semverResult = semver.rcompare(versionA, versionB);
+    var semverResult = semver.compare(versionA, versionB);
     if (semverResult !== 0) {
         return semverResult;
     }
     var revA = getRev(versionA);
     var revB = getRev(versionB);
     if (revA !== revB) {
-        return revB - revA;
+        return revA > revB ? 1 : -1;
     }
     var devA = Number(isDevelopmentVersion(versionA));
     var devB = Number(isDevelopmentVersion(versionB));
     if (devA !== devB) {
-        return devA - devB;
+        return devA > devB ? -1 : 1;
     }
     return versionA.localeCompare(versionB);
 }, function (a, b) { return a + " && " + b; });
@@ -173,4 +177,29 @@ exports.prerelease = function (version) {
     }
     version = trimOsText(safeSemver(version));
     return semver.prerelease(version);
+};
+/**
+ * @summary Check if a version is greater than or equal to another
+ * @name gte
+ * @public
+ * @function
+ *
+ * @description Returns true if versionA is greater than or equal to versionB.
+ * Valid semver versions are always weighted above non semver strings.
+ * Non-semver strings are compared alphabetically.
+ *
+ * @param {string|null} versionA - The version string to compare against
+ * @param {string|null} versionB - The version string to compare to versionA
+ *
+ * @returns {boolean} - true if versionA is greater than or equal to versionB, otherwise false.
+ *
+ * @example
+ * resinSemver.gte('2.0.5', '1.16.0'); //--> true
+ *
+ * resinSemver.gte('Resin OS 2.0.5', 'Resin OS 2.0.2+rev2'); //--> true
+ *
+ * resinSemver.gte('1.16.0', 'Resin OS 2.0.2+rev2'); //--> false
+ */
+exports.gte = function (versionA, versionB) {
+    return exports.compare(versionA, versionB) >= 0;
 };
